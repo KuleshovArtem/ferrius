fc515d33eb31ff698a52163dd33029d60573f674
artem kuleshov <kooleshov.artem@gmail.com>
2022-11-26 00:38:29 +0500

    исправлены ошибки, сделана адаптивная верстка для планшетов

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 const slider = tns({
     container: '.production__slider',
-    items: 2,
+    items: 1,
     slideBy: 'page',
     autoplay: true,
     autoplayButtonOutput: false,
@@ -8,6 +8,14 @@ const slider = tns({
     edgePadding: 10,
     nav: false,
     controls: false,
+    responsive: {
+        767: {
+          items: 2
+        },
+        // 900: {
+        //     item: 1
+        // }
+      }
 });
 
 document.querySelector('.next').addEventListener('click', function () {
@@ -16,4 +24,24 @@ document.querySelector('.next').addEventListener('click', function () {
 
 document.querySelector('.prev').addEventListener('click', function () {
     slider.goTo('prev');
+});
+
+//menu hamburger
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu__wrapper'),
+    menuItem = document.querySelectorAll('.menu__item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger__active');
+        menu.classList.toggle('menu__active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger__active');
+            menu.classList.toggle('menu__active');
+        });
+    });
 });
